f159acdfce34b0d8e3d9c3304f8892aa7801adcc
bgiorgio0506 <40486332+bgiorgio0506@users.noreply.github.com>
2018-10-24 22:41:10 +0300

    Add files via upload
    [FIXED] Distance logic

--- api.php
+++ api.php
@@ -1,14 +1,14 @@
 <?php
-
 const DUPLICATE_ERROR = "Errore: ricarica la pagina.";
+
 
 $db_giorgio = array('servername' => "techaviationflight.com.mysql:3306", 'username' => 'techaviationflight_com_whatwefly', 'password' => "Marino01.");
 $db_gio = array('servername' => 'localhost', 'username' => 'root', 'password' => '');
 
-foreach ($db_gio as $key => $value) {
-	$sn = $db_gio['servername'];
-	$un = $db_gio['username'];
-	$pwd =  $db_gio['password'];
+foreach ($db_giorgio as $key => $value) {
+	$sn = $db_giorgio['servername'];
+	$un = $db_giorgio['username'];
+	$pwd =  $db_giorgio['password'];
 }
 
 $conn = new mysqli($sn, $un, $pwd);
@@ -17,7 +17,7 @@ if (!$conn)
 die("Connection failed: " . mysqli_connect_error());
 
 #mysqli_select_db($conn , "techaviationflight_com_whatwefly");
-mysqli_select_db($conn, "whatwefly");
+mysqli_select_db($conn, "techaviationflight_com_whatwefly");
 
 function redir($sts){
 	if (!$sts) header("Location: index.php?robin=".$_GET['robin']."&abroad=".$_GET['abroad']."&fmdup=1");
@@ -47,60 +47,21 @@ function getICAOfromSTATE($conn, $state){
 		return $a['icao'];
 	}
 }
-function getDeplatCoordinates($conn, $dep)
-{
-	$sql = 'SELECT latitude_deg FROM airport_list WHERE ident = "' . $dep . '" ';
-	$r = mysqli_query($conn, $sql);
-	while ($a = mysqli_fetch_assoc($r)) {
-		return $a['start_lat'];
 
-	}
-}
-function getDeplongCoordinates($conn, $dep)
-{
-	$sql = 'SELECT longitude_deg FROM airport_list WHERE ident = "' . $dep . '" ';
-	$r = mysqli_query($conn, $sql);
-	while ($a = mysqli_fetch_assoc($r)) {
-		return $a['start_long'];
-	}
-}
-function getArrlatCoordinates($conn, $arr)
-{
-	$sql = 'SELECT latitude_deg FROM airport_list WHERE ident = "' . $arr . '" ';
-	$r = mysqli_query($conn, $sql);
-	while ($a = mysqli_fetch_assoc($r)) {
-		return $a['end_lat'];
-
-	}
-}
-function getArrlongCoordinates($conn, $arr)
-{
-	$sql = 'SELECT longitude_deg FROM airport_list WHERE ident = "' . $arr . '" ';
-	$r = mysqli_query($conn, $sql);
-	while ($a = mysqli_fetch_assoc($r)) {
-		return $a['end_long'];
-	}
-}
 function preventDuplicate ($icao1, $icao2) {
 	return (($icao1 == $icao2)?false:true);
 }
-function getDistance($start_long, $start_lat, $end_long, $end_lat) {
-	$t = $start_long - $end_lat;
-	$distance= sin(degree_to_radious($start_lat)) * sin(degree_to_radious($end_lat)) * cos(degree_to_radious($start_long)) * cos(degree_to_radious($end_long)) * cos(degree_to_radious($t));
-	$distance = acos($distance);
-	$distance = radious_to_degree($distance);
-	$distance = $distance * 60 * 1.1515;
-	$distance = $distance * 1.1507;
-	$distance = number_format($distance, 1);
-	return $distance;
-}
 
-function degree_to_radious($deg){
-	return($deg * pi()/180.0);
-}
-
-function radious_to_degree($rad){
-	return($rad/pi() * 180.0);
+function getDistance($start_long, $start_lat, $end_long, $end_lat){
+    $d1 = sin((deg2rad($end_lat)- deg2rad($start_lat))/2);
+    $d1 = pow($d1, 2);
+    $d2 = cos(deg2rad($end_lat)) * cos(deg2rad($start_lat));
+		$d3 = sin((deg2rad($end_long)- deg2rad($start_long))/2);
+		$d3 = pow($d3, 2);
+		$r1 = $d1 + $d2 * $d3;
+    $distance = 2 * 6371 * asin(sqrt($r1));
+    $distance = $distance / 1.85200;
+    return $distance;
 }
 
 function showDepArr($dep, $arr){
@@ -127,10 +88,11 @@ function showDistance($conn, $dep, $arr){
 	}
 
 	$distance = getDistance(floatval($lon_dep), floatval($lat_dep), floatval($lon_arr), floatval($lat_arr));
-	echo '<br>Distanza: '. $distance;
+	echo ' distanza: '. $distance;
 }
 
 if (isset($_GET['abroad'])) {
+
 
 	switch ($_GET['abroad']) {
 		case 'si':
@@ -173,6 +135,42 @@ if (isset($_GET['abroad'])) {
 			break;
 	}
 
+/* CONDIZIONI ERRATE!!!
+
+	if ($_GET['abroad'] == "no" && $_GET['robin'] == "si"){
+		//vola in italia round robin
+		$icao = getICAOfromIT($conn);
+		echo 'TRATTA: ' . $icao . ' - ' . $icao . '';
+		echo 'Distanza: /';
+	}
+	elseif ($_GET['abroad'] == "no" && $_GET['robin'] == "no"){
+		//vola in italia normale
+		$a = getICAOfromIT($conn);
+		$b = getICAOfromIT($conn);
+		if (preventDuplicate($a, $b)) {
+			showDepArr($a, $b);
+			showDistance($conn, $a, $b);
+		}
+		else redir(false);
+	}
+	else {
+		//volo estero round robin
+		$icao = getICAOfromABROAD($conn);
+		echo 'TRATTA: '.$icao.' - '.$icao.'';
+		echo 'Distanza: /';
+	}
+}
+
+if ($_GET['abroad']=="si" && $_GET['robin'=="no"]){
+	//volo esterno normale
+	$a = getICAOfromABROAD($conn);
+	$b = getICAOfromABROAD($conn);
+	if (preventDuplicate($a, $b)) {
+		showDepArr($a, $b);
+		showDistance($conn, $a, $b);
+	}
+	else redir(false);
+	*/
 }
 
 
